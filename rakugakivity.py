8dbfcd9edaaa154801012b9488438da311e8b23b
kimihiro_n <kimihiro.nisimura@gmail.com>
2019-08-15 12:31:26 +0000

    :+1: Add format

--- rakugakivity.py
+++ rakugakivity.py
@@ -1,4 +1,6 @@
-from typing import NamedTuple
+import argparse
+
+from typing import NamedTuple, List
 from datetime import datetime
 
 from twitter_scraper import get_tweets
@@ -6,6 +8,7 @@ from twitter_scraper import get_tweets
 
 class Tweet(NamedTuple):
     id: int
+    text: str
     created_at: datetime
     images: str
     retweets: int
@@ -15,6 +18,7 @@ class Tweet(NamedTuple):
     def from_dict(cls, d: dict):
         return cls(
             id=d['tweetId'],
+            text=d['text'],
             created_at=d['time'],
             images=d['entries']['photos'],
             retweets=d['retweets'],
@@ -27,11 +31,34 @@ class UserTweets():
     def __init__(self, username):
         self.username = username
 
-    def fetch_all(self):
+    def fetch_all(self) -> List[Tweet]:
         page = 1
-        ret = []
-        try:
-            tweets = get_tweets(self.username, pages=page)
-            ret.extend([Tweet.from_dict(t) for t in tweets])
-        finally:
-            return ret
+
+        while True:
+            try:
+                tweets = get_tweets(self.username, pages=page)
+                for tweet in tweets:
+                    if tweet['isRetweet']:
+                        continue
+                    tw = Tweet.from_dict(tweet)
+                    if tw.images:
+                        yield tw
+                page += 1
+            except Exception as e:
+                print(e)
+                break
+
+
+def format_as_csv(tweets: List[Tweet]) -> str:
+    return f'{tw.created_at.isoformat()}, {tw.likes}, {tw.retweets}, {tw.images[0]}'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Analyze your rakugaki tweet.')
+    parser.add_argument('username', help='twitter user name(without @)')
+    args = parser.parse_args()
+
+    print('created_at, like, rt, image')
+
+    for tw in UserTweets(args.username).fetch_all():
+        print(format_as_csv(tw))
